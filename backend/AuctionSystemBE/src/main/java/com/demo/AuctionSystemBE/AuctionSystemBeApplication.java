7666ffebf58e50fbaabeef687bdd9a028387d9dc
MihaiOsan <mihai.osan01@e-uvt.ro>
2022-12-07 19:02:19 +0200

    Spring files added

---
+++ backend/AuctionSystemBE/src/main/java/com/demo/AuctionSystemBE/AuctionSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.AuctionSystemBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionSystemBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionSystemBeApplication.class, args);
+	}
+
+}
