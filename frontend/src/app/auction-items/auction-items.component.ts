3ed152f77cfe762171862870329782e0c1e26c94
antoniuprata <antoniu.prata01@e-uvt.ro>
2022-12-12 22:11:25 +0200

    Auction items component

---
+++ frontend/src/app/auction-items/auction-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auction-items',
+  templateUrl: './auction-items.component.html',
+  styleUrls: ['./auction-items.component.scss']
+})
+export class AuctionItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
