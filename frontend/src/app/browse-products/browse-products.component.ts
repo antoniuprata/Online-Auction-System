26b8e1092ccdd65052c9d7260610920764b38cdc
antoniuprata <antoniu.prata01@e-uvt.ro>
2022-12-12 22:12:07 +0200

    Added Home, HowItWorks, AboutUs, Browse

---
+++ frontend/src/app/browse-products/browse-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'browse-products',
+  templateUrl: './browse-products.component.html',
+  styleUrls: ['./browse-products.component.scss']
+})
+export class BrowseProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
