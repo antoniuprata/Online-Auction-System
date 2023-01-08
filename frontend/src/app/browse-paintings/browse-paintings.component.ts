683510dd783bd0832718c1b2ccf33d186510a27d
antoniuprata <antoniu.prata01@e-uvt.ro>
2023-01-08 13:50:43 +0200

    Login/Register design changes

---
+++ frontend/src/app/browse-paintings/browse-paintings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-paintings',
+  templateUrl: './browse-paintings.component.html',
+  styleUrls: ['./browse-paintings.component.scss']
+})
+export class BrowsePaintingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ frontend/src/app/browse-sculptures/browse-sculptures.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-sculptures',
+  templateUrl: './browse-sculptures.component.html',
+  styleUrls: ['./browse-sculptures.component.scss']
+})
+export class BrowseSculpturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
