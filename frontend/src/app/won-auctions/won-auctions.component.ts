27b41a16675718fb67cd52a25e3d6dbf7d57e240
antoniuprata <antoniu.prata01@e-uvt.ro>
2023-01-17 16:06:08 +0200

    finishing commit

--- frontend/src/app/won-auctions/won-auctions.component.ts
+++ frontend/src/app/won-auctions/won-auctions.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
+import { AuctionItem } from '../model';
 
 @Component({
   selector: 'app-won-auctions',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./won-auctions.component.scss']
 })
 export class WonAuctionsComponent implements OnInit {
+  auctionItems: any;
+  currentUser: any;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private router: Router, private authentificationService: AuthenticationService) { }
 
   ngOnInit(): void {
+    this.currentUser = this.authentificationService.currentUserValue;
+    this.getWonItems();
+    console.log(this.auctionItems);
   }
 
+  getWonItems() {
+    return this.httpClient.post<any>('http://localhost:8080/product/wonProducts', {emailUser: this.currentUser.email}, { responseType: 'json'})
+      .subscribe((data) => {
+        console.log(data);
+        this.auctionItems = data;
+        console.log('AUCTION ITEMSs', this.auctionItems);
+      });
+  }
+
+  viewListing(id: number) {
+    this.router.navigate(['/view-listing', id]);
+  }
 }
